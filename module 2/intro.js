f33433fa4606b36205da80aff475fe71a9db8c9b
SaurabhVish09 <saurabh@Saurabhs-MacBook-Air.local>
2022-04-07 11:34:10 +0530

    intro

---
+++ module 2/intro.js
@@ -0,0 +1,25 @@
+console.log("this is my pc");
+for ( i = 1 ; i <= 5; i++){
+    console.log(i)
+}
+let count = 12
+while(count>0){
+    console.log(count);
+    count--
+} 
+let num = 18;
+let isprime = true;
+
+for (i = 2; i*i < num ; i++){
+    if (num % i == 0){
+        isprime = false;
+   }
+}
+if (isprime == true){
+    console.log("number is  prime");
+
+}
+else{
+    console.log("number is not prime")
+}
+   
